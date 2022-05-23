9a423c6b2d95eabda60fe0318dc8acf4b69c0a63
Richard Price <richyprice77@gmail.com>
2022-05-23 13:29:10 +0100

    inital commit

---
+++ SwiftUIIRichyExpenses/SwiftUIIRichyExpensesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIIRichyExpensesApp.swift
+//  SwiftUIIRichyExpenses
+//
+//  Created by Richard Price on 23/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIIRichyExpensesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
